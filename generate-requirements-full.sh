237038c97ba9ed1b6b00a1ae8f2dca6462121936
Gabe Mendoza <gmendoza@civisanalytics.com>
2023-09-06 12:46:11 -0500

    should be good to go. pulling current pip dependencies from latest 7.x.x datascience-python container

--- generate-requirements-full.sh
+++ generate-requirements-full.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
-pip install pip-tools
-pip-compile --output-file=requirements-full.txt --pip-args='--prefer-binary' requirements-core.txt
+docker run \
+  -v $(pwd):/src \
+  -ti --rm civisanalytics/datascience-python:7 \
+  /bin/bash -c \
+    "pip freeze >> /tmp/requirements-aggregated-core.txt && \
+    cat /src/requirements-core.txt >> /tmp/requirements-aggregated-core.txt && \
+    pip install pip-tools && \
+    pip-compile --output-file=/src/requirements-full.txt --pip-args='--prefer-binary' /tmp/requirements-aggregated-core.txt"
