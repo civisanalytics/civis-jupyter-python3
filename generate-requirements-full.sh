a1aee9fe5af9df199810d3f941ea9b6d30b011df
Michael Heilman <mheilman@civisanalytics.com>
2024-08-26 14:49:52 -0500

    update requirements.txt

--- generate-requirements-full.sh
+++ generate-requirements-full.sh
@@ -2,7 +2,7 @@
 DS_PYTHON_IMG_VERSION=$(head -n 1 ./.ds_python_version)
 docker run \
   -v $(pwd):/src \
-  -ti --rm civisanalytics/datascience-python:${DS_PYTHON_IMG_VERSION} \
+  -ti --rm --platform=linux/x86_64 civisanalytics/datascience-python:${DS_PYTHON_IMG_VERSION} \
   /bin/bash -c \
     "pip freeze >> /tmp/requirements-aggregated-core.txt && \
     cat /src/requirements-core.txt >> /tmp/requirements-aggregated-core.txt && \
