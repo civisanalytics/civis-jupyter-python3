79f99599f34b77394b5e274e2d5bde10e0265bf3
Jackson Lee <86482098+jacksonlee-civis@users.noreply.github.com>
2025-07-09 09:43:48 -0400

    [CIVIS-11075] ENH update datascience-python base image to v8.3.0 (#62)
    * ENH update to ds-py image v8.3.0
    
    * ENH refresh requirements-full.txt

--- generate-requirements-full.sh
+++ generate-requirements-full.sh
@@ -4,7 +4,6 @@ docker run \
   -v $(pwd):/src \
   -ti --rm civisanalytics/datascience-python:${DS_PYTHON_IMG_VERSION} \
   /bin/bash -c \
-    "pip freeze >> /tmp/requirements-aggregated-core.txt && \
+    "uv pip freeze >> /tmp/requirements-aggregated-core.txt && \
     cat /src/requirements-core.txt >> /tmp/requirements-aggregated-core.txt && \
-    pip install pip-tools && \
-    pip-compile --output-file=/src/requirements-full.txt --strip-extras --pip-args='--prefer-binary' --upgrade /tmp/requirements-aggregated-core.txt"
+    uv pip compile --output-file=/src/requirements-full.txt --upgrade /tmp/requirements-aggregated-core.txt"
